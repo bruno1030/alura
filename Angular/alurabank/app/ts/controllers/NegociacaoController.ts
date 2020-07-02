4832b0b833a6305ef1119ba002cf45cf4feb709c
Bruno Martins <brunomartins.1030@gmail.com>
2020-07-02 11:49:30 -0300

    type of attributes

--- Angular/alurabank/app/ts/controllers/NegociacaoController.ts
+++ Angular/alurabank/app/ts/controllers/NegociacaoController.ts
@@ -2,23 +2,25 @@
 
 class NegociacaoController{
 
-    private _inputData;  // eh o elemento do DOM que corresponde ao input da data
-    private _inputQuantidade; // eh o elemento do DOM que corresponde ao input da quantidade
-    private _inputValor;   // tanto o private como o underline me ajudam para que sejam atributos privados, ninguem tenha acesso fora da classe. Pode ser soh o private ou o underline, mas eh bom sim usar os dois, seguir a convencao que fala do underline. Mas em tese soh o private ja seria suficiente
+    private _inputData: HTMLInputElement;  // eh o elemento do DOM que corresponde ao input da data
+    private _inputQuantidade: HTMLInputElement; // eh o elemento do DOM que corresponde ao input da quantidade
+    private _inputValor: HTMLInputElement;   // tanto o private como o underline me ajudam para que sejam atributos privados, ninguem tenha acesso fora da classe. Pode ser soh o private ou o underline, mas eh bom sim usar os dois, seguir a convencao que fala do underline. Mas em tese soh o private ja seria suficiente
 
     constructor(){
-
-        this._inputData = document.querySelector('#data');      // dentro dos parenteses eh o id do meu HTML, e la o id esta com esse nome "data"
-        this._inputQuantidade = document.querySelector('#quantidade');
-        this._inputValor = document.querySelector('#valor');
+        //aqui embaixo vamos fazer o casting (usando <>), que eh para transformar um cara mais generico (element do querySelector) para um tipo mais especifico (HTMLInputElement)
+        this._inputData = <HTMLInputElement>document.querySelector('#data');      // dentro dos parenteses eh o id do meu HTML, e la o id esta com esse nome "data"
+        this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
+        this._inputValor = <HTMLInputElement>document.querySelector('#valor');
     }
 
-    adiciona(event){
+    adiciona(event: Event){
+
         event.preventDefault();
+
         const negociacao = new Negociacao(
-            this._inputData.value,
-            this._inputQuantidade.value,
-            this._inputValor.value
+            new Date(this._inputData.value.replace(/-/g, ',')),   // dentro do ultimo parenteses, quer dizer que vai substituir todas os hifens dentro da String do meu input data, por virgula.
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
         );
 
         console.log(negociacao);
--- Angular/alurabank/app/ts/models/Negociacao.ts
+++ Angular/alurabank/app/ts/models/Negociacao.ts
@@ -1,14 +1,8 @@
 class Negociacao{
 
-    // declaracao das propriedades de classe
-    private _data;
-    private _quantidade;
-    private _valor;
 
-    constructor(data, quantidade, valor){
-        this._data = data;
-        this._quantidade = quantidade;              // esse underline eh uma js, que essas propriedades nao podem ser alteradas fora de metodos da propria classe. Por isso vou criar os gets, os metodos ali embaixo para que eu possa utilizar e alterar essas propriedades
-        this._valor = valor;                        // o valor depois do igual eh o valor que vou receber da tela
+    constructor(private _data: Date, private _quantidade: number, private _valor: number){
+
     }
 
     get data(){
